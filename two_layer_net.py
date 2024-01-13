1f08d53f399c8735424f2c07bf3aed5607107633
Kai Washino <79495772+Kai-Washino@users.noreply.github.com>
2024-01-13 19:38:40 +0900

    Update Backpropagation from Numerical

--- two_layer_net.py
+++ two_layer_net.py
@@ -1,6 +1,8 @@
 import numpy as np
 from method import *
 from grad import numerical_gradient
+from layers import *
+from collections import OrderedDict
 
 class TwoLayerNet:
     def __init__ (self, input_size, hidden_size, output_size, weight_init_std = 0.01):
@@ -9,17 +11,27 @@ class TwoLayerNet:
         self.params['b1'] = np.zeros(hidden_size)
         self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
         self.params['b2'] = np.zeros(output_size)
+
+        self.layers = OrderedDict()
+        self.layers['Affine1'] = Affine(self.params['W1', self.params['b1']])
+        self.layers['Relu1'] = Relu()
+        self.layers['Affine2'] = Affine(self.params['W2'], self.params['b2'])
+
+        self.lastLayer = SoftmaxWithLoss()
     
     def predict(self, x):
-        W1, W2 = self.params['W1'], self.params['W2']
-        b1, b2 = self.params['b1'], self.params['b2']
+        for layer in self.layers.values():
+            x = layer.forward()
+        return x
+        # W1, W2 = self.params['W1'], self.params['W2']
+        # b1, b2 = self.params['b1'], self.params['b2']
 
-        a1 = np.dot(x, W1) + b1
-        z1 = sigmoid(a1)
-        a2 = np.dot(z1, W2) + b2
-        y = softmax(a2)
+        # a1 = np.dot(x, W1) + b1
+        # z1 = sigmoid(a1)
+        # a2 = np.dot(z1, W2) + b2
+        # y = softmax(a2)
 
-        return y
+        # return y
     
     def loss(self, x, t):
         y = self.predict(x)
@@ -28,7 +40,7 @@ class TwoLayerNet:
     def accuracy(self, x, t):
         y = self.predict(x)
         y = np.argmax(y, axis = 1)
-        t = np.argmax(t, axis=1)
+        if t.ndim != 1 : t = np.argmax(t, axis=1)
 
         accuracy = np.sum(y == t) / float(x.shape[0])
         return accuracy
@@ -41,4 +53,22 @@ class TwoLayerNet:
         grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
         grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
         
+        return grads
+    
+    def gradient(self, x, t):
+        self.loss(x, t)
+        dout = 1
+        dout = self.lastLayer.backward(dout)
+
+        layers = list(self, layers.values())
+        layers.reverse
+        for layer in layers:
+            dout = layer.backward(dout)
+        
+        grads = {}
+        grads['W1'] = self.layers['Affine1'].dw
+        grads['b1'] = self.layers['Affine1'].db
+        grads['W2'] = self.layers['Affine2'].dw
+        grads['b2'] = self.layers['Affine2'].db
+
         return grads
